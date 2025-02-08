edb0acbfe2a3ec60f27d2ecb28e18a22986fe72b
Meharab-Islam <mdnibir178@gmail.com>
2025-02-08 23:32:14 +0600

    some data added

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Dynamic Font Size App ',
+          title: 'Demo App ',
           theme: ThemeData(
             primaryColor: AppColor.primaryColor,
             primarySwatch: Colors.blue,
